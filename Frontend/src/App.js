d57cc323ff99b7e1a0c811ec4f829a46271ec49e
Revelen <revelenxd@gmail.com>
2022-05-16 05:14:04 +0200

    frontend add

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{ useState } from 'react';
+import Login from './components/login';
+import CreateAccount from './components/createAccount';
+import UserMenu from './components/UserMenu';
+import ModMenu from './components/ModMenu'
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    
+    const [swi, setSwi] = useState({ status:"login"});
+    
 
+
+    switch (swi.status) {
+        case "login":
+            return (
+                <div>
+                    <Login setSwi={setSwi} />
+                </div>
+            );
+            
+        case "goodLoginU":
+            return (
+                <div>
+                    <UserMenu user={swi.user} />
+                </div>
+            );
+        case "goodLoginM":
+            return (
+                <div>
+                    
+                    <ModMenu user={swi.user}/>
+                </div>
+            );
+
+        case "createAcc":
+            return (
+                <div>
+                    <CreateAccount setSwi={setSwi} />
+                </div>
+            );
+        case "sucCreAcc":
+            return (
+                <div>
+                    <h1>Account created</h1>
+                    <button className="btn" onClick={() => { setSwi({status:"login"})}}>Sign in</button>
+                </div>
+                )
+        default:
+            console.log("ee lipa jest")
+    }
+    }
+
+
+
+
+//    if (logged.status === false) {
+//        return (
+//            <div>
+//                <Login setSwi={setSwi}/>
+//            </div>
+//        );
+//    }
+//    else {
+//        return (
+//            <div>
+//                <h1>Zalogowano {logged.user.email}</h1>
+//            </div>
+//        );
+//    }
+      
+//};
+    
 export default App;
