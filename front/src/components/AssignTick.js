dd398b1331093e2f6aa2e23e6b6f9b826efcd3d9
Revelen <revelenxd@gmail.com>
2022-05-17 09:24:53 +0200

    new icon/title

--- front/src/components/AssignTick.js
+++ front/src/components/AssignTick.js
@@ -4,7 +4,7 @@ function AssignTick(props) {
 
     const [data, setData] = useState({ tickets: [], mess: 1 });
 
-    useEffect((props) => {
+    useEffect(() => {
         fetch('/api/ticket/own', {
             method: 'POST', headers: { 'Content-Type': 'application/json' }, body: JSON.stringify(props.user)
         })
